7757edaa729c0e423873519fdd35a62c53731497
HeYuan919 <heyuan.huang@yale.edu>
2023-04-21 18:50:01 -0400

    add gpt3.5-turbo max_tokens=128 output

--- gpt.py
+++ gpt.py
@@ -6,14 +6,17 @@ from datasets import Dataset, DatasetDict, load_metric, load_dataset
 import argparse
 import os
 
-os.environ["OPENAI_API_KEY"] = " "
+os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-DATASET_NAME = "turkcorpus"
+# DATASET_NAME = "turkcorpus"
+DATASET_NAME = "radiology_indiv"
+# DATASET_NAME = "cochrane"
+# DATASET_NAME = "asset"
 dataset = load_dataset(
     "json", data_files=f"data/{DATASET_NAME}_multiple.json", field="test"
 )["train"]
-test_output = []
+# test_output = []
 for text in dataset["input"]:
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -22,16 +25,20 @@ for text in dataset["input"]:
                 "role": "system",
                 "content": "You are a helpful assistant that simplifies text.",
             },
-            {"role": "user", "content": f'Simplify this text: "{text}"'},
+            {
+                "role": "user",
+                "content": f"Simplify this text for a fourth grade or lower student to understand: {text}",
+            },
         ],
         temperature=0,
-        max_tokens=256,
+        max_tokens=128,
     )
 
     simplified_sen = response["choices"][0]["message"]["content"]
-    test_output.append(simplified_sen)
+    # test_output.append(simplified_sen)
     # open file in write mode
-with open(f"output/{DATASET_NAME}_gpt3.txt", "w") as fp:
-    for item in test_output:
-        fp.write("%s\n" % item)
-    print("Done")
+    with open(f"output/{DATASET_NAME}_gpt3_turbo_128.txt", "a") as fp:
+        # for item in test_output:
+        fp.write("%s\n" % simplified_sen)
+
+print(f"{DATASET_NAME} Done")
