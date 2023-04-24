6e9bf997b166091cba4eec761dc9e98a21d684b4
HeYuan919 <heyuan.huang@yale.edu>
2023-04-24 17:12:51 -0400

    update questeval result and context output

--- gpt.py
+++ gpt.py
@@ -7,19 +7,19 @@ import argparse
 import os
 import re
 
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-DATASET_NAME = "turkcorpus"
-# DATASET_NAME = "radiology_indiv"
-# DATASET_NAME = "cochrane"
-# DATASET_NAME = "asset"
+# DATASET_NAME = "turkcorpus_context_all"
+DATASET_NAME = "radiology_indiv_context_some"
+# DATASET_NAME = "cochrane_context_some"
+# DATASET_NAME = "asset_context_all"
 dataset = load_dataset(
     "json", data_files=f"data/{DATASET_NAME}_multiple.json", field="test"
 )["train"]
 # test_output = []
-for text in dataset["input"]:
+for text in dataset["input"][540:]:
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
